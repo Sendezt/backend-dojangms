7e0f70e3881df6c91719092377c8d5c9a3e995ce
Juhio <bimaafka14@gmail.com>
2026-06-25 21:01:52 +0700

    test deploy

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.get("/", (req, res) => {
   res.json({
     status: true,
-    message: "Server is running successfully, welcome to the API Test Deploy",
+    message: "Server is running successfully, welcome to the API DojangMS",
     endpoint: ["/api/auth", "/api/admin", "/api/public"],
   });
 });
